1e345e128f7367f587eb34b89090cfaeb5cdd6e6
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-12-29 13:09:16 +0530

    touchup

--- packages/persist-and-sync/touchup.js
+++ packages/persist-and-sync/touchup.js
@@ -42,3 +42,5 @@ fs.writeFileSync(
 	path.resolve(__dirname, "dist", "package.json"),
 	JSON.stringify(packageJson, null, 2),
 );
+
+fs.copyFileSync(path.resolve(__dirname, "..","..", "README.md"), path.resolve(__dirname, "dist", "README.md"));
